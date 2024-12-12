5526955049a390165da58095cee22257f57ca9c2
andresp10 <andrespinzon0904@gmail.com>
2024-12-12 16:04:27 -0500

    added img alt text

--- scripts/index.js
+++ scripts/index.js
@@ -50,6 +50,7 @@ function getCardElement(data) {
 
   cardNameElement.textContent = data.name;
   cardImageElement.src = data.link;
+  cardImageElement.alt = data.name;
 
   return cardElement;
 }
@@ -57,11 +58,11 @@ function getCardElement(data) {
 function openModal() {
   editModalNameInput.value = profileName.textContent;
   editModalDescriptionInput.value = profileDescription.textContent;
-  editModal.classList.add("modal__opened");
+  editModal.classList.add("modal_opened");
 }
 
 function closeModal() {
-  editModal.classList.remove("modal__opened");
+  editModal.classList.remove("modal_opened");
 }
 
 function handleEditFormSubmit(evt) {
